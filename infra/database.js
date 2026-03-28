55241ca97df1825a1a714267b0002057b2a0feb4
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-03-28 13:36:31 -0300

    Melhorando o database ao lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -1,3 +1,4 @@
+import { getVersion } from 'jest';
 import { Client } from 'pg';
 
 async function query(queryObject){
@@ -9,12 +10,16 @@ async function query(queryObject){
         password: process.env.POSTGRES_PASSWORD
     });
     await client.connect();
-    const result = await client.query(queryObject);
-    await client.end();
-
-    return result;
+    try{
+        const result = await client.query(queryObject);
+        return result;
+    }catch(error){
+        console.error(error);
+    }finally{
+          await client.end();
+    }
+    
 }
-
 export default{
-    query:query
+    query:query,
 }
