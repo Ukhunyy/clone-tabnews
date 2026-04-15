561aa7493947c243c5a27b2c54561358a703389c
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-04-14 21:58:39 -0300

    Adds api/v1/migrations end point

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,32 +1,34 @@
-import database from "infra/database.js"
+import database from "/infra/database.js";
 
 async function status(req, res) {
-  const updateAt= new Date().toISOString();
+  const updateAt = new Date().toISOString();
 
-  const dataBaseVersionResult = await database.query("SHOW server_version;")
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = dataBaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
-  const databaseMaxConnectionsValues = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValues =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query(
-    {
-      text:"SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [databaseName]
-    }
-  )
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
-    update_at:updateAt,
+    update_at: updateAt,
     dependecies: {
       database: {
-        version:databaseVersionValue,
+        version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValues),
-       opned_connections:databaseOpenedConnectionsValue,
-      }
-    }
+        opned_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,26 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(res1.status).toBe(201);
+
+  const responseBody = await res1.json();
+
+  const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  const getBody = await res2.json();
+  expect(getBody.length).toBe(0);
+  expect(res2.status).toBe(200);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
