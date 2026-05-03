265d74598a95296a00d43ff200409c603ea50267
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-05-03 14:55:50 -0300

    test: rename all desciptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,22 +6,30 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(res1.status).toBe(201);
+
+        const responseBody = await res1.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+
+        const getBody = await res2.json();
+        expect(getBody.length).toBe(0);
+        expect(res2.status).toBe(200);
+      });
+    });
   });
-  expect(res1.status).toBe(201);
-
-  const responseBody = await res1.json();
-
-  const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  const getBody = await res2.json();
-  expect(getBody.length).toBe(0);
-  expect(res2.status).toBe(200);
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatAt = new Date(responseBody.update_at).toISOString();
-  //Se o valor que chegar em update_at for diferente do parsedUpdateAt, o teste dá erro
-  expect(responseBody.update_at).toEqual(parsedUpdatAt);
-  expect(responseBody.dependecies.database.version).toEqual("16.13");
-  expect(responseBody.dependecies.database.max_connections).toEqual(100);
-  expect(responseBody.dependecies.database.opned_connections).toEqual(1);
+      const parsedUpdatAt = new Date(responseBody.update_at).toISOString();
+      //Se o valor que chegar em update_at for diferente do parsedUpdateAt, o teste dá erro
+      expect(responseBody.update_at).toEqual(parsedUpdatAt);
+      expect(responseBody.dependecies.database.version).toEqual("16.13");
+      expect(responseBody.dependecies.database.max_connections).toEqual(100);
+      expect(responseBody.dependecies.database.opned_connections).toEqual(1);
+    });
+  });
 });
